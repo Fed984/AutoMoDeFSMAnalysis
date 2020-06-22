b83aea4ae3dbcc5c26f8434d63bddfa25682b64a
Federico Pagnozzi <federico.pagnozzi@ulb.ac.be>
2020-06-22 18:02:45 +0200

    another little change to the calculation of the probability for the behavior policy in the importance sampling calculation

--- AutoMoDeFSMExperiment.py
+++ AutoMoDeFSMExperiment.py
@@ -309,8 +309,8 @@ class AutoMoDeExperiment:
 					prob_transition = 1.0
 					if(idx > 0):
 						previous_state = episode[0][idx-1] # previous state		
-						prob_transition = old_fsm[previous_state].prob_of_reaching_state(state, new_fsm,tr_neighbors[idx],tr_ground[idx])
-						#prob_transition2 = float(tr_prob[idx]/tr_actives[idx]) # the measured probability of coming to the current state
+						#prob_transition = old_fsm[previous_state].prob_of_reaching_state(state, new_fsm,tr_neighbors[idx],tr_ground[idx])
+						prob_transition = float(tr_prob[idx]/tr_actives[idx]) # the measured probability of coming to the current state
 						prob_transition_target = new_fsm[previous_state].prob_of_reaching_state(state, new_fsm,tr_neighbors[idx],tr_ground[idx]) # probability that the target policy transitions from the previous state to the current state
 						#if(previous_state == 2 ):
 						#print("Transition from {0} to {1} : old probability {2} - {3} new probability  {4}".format(previous_state,state,prob_transition,prob_transition2,prob_transition_target))	
